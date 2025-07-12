9dd50ea1486fdcf87eb0a211aa6fb0cadb4938c0
md junaid qamar <junaidqamar8753@gmil.com>
2025-07-12 19:59:24 +0530

    frontend update

--- server.js
+++ server.js
@@ -4,19 +4,29 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const socketIO = require("socket.io");
+
 dotenv.config();
 connectDB();
 
 const app = express();
 const server = http.createServer(app);
+
+const corsOptions = {
+  origin: "https://todo-webalar.netlify.app",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+
+
 const io = socketIO(server, {
-  cors: {
-    origin: "*",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-  },
+  cors: corsOptions,
 });
 
 app.set("io", io);
+
 io.on("connection", (socket) => {
   console.log("🟢 User connected:", socket.id);
 
@@ -37,31 +47,11 @@ io.on("connection", (socket) => {
   });
 });
 
-
-
-app.use(cors());
-app.use(express.json());
-
-// Socket.IO setup
-io.on("connection", (socket) => {
-  console.log("Socket connected:", socket.id);
-
-  socket.on("disconnect", () => {
-    console.log("Socket disconnected:", socket.id);
-  });
-});
-
-// Routes
 app.use("/api/auth", require("./routes/authRoutes"));
-// app.use("/api/tasks", require("./routes/taskRoutes"));
 app.use("/api/tasks", require("./routes/taskRoutes"));
 
 
-
 const PORT = process.env.PORT || 5000;
-// const PORT = process.env.PORT || 5000;
-
 server.listen(PORT, () => {
   console.log(`✅ Server running on port ${PORT}`);
 });
-
